55e9187d285a9b95c5f37ad516c24ea8716417c8
meetoza30 <meetoz305@gmail.com>
2024-07-24 21:24:30 +0530

    changes in header & footer

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,19 +10,19 @@ function Header(){
         <>
         {/* <div className="text-black font-bold size-10">Hello! Welcome</div> */}
         {/* <img src={TechLabLogo} className=" h-20 w-20"/> */}
-        <nav className="flex flex-row items-center justify-around bg-orange-600 headcontain max-w-screen h-30 shadow-md shadow-yellow-600">
+        <nav className="items-center navbar  bg-orange-600 h-30 shadow-md shadow-yellow-600">
             <a href="/">
                 <img src={TechLabLogo} className="tlogo"/>
             </a>
             <div className="flex flex-row justify-between items-center middiv">
-            <Link style={{ textDecoration: 'none' }} to="home" className="mx-3 bg-yellow-200 rounded-md px-2 py-1 text-black font-semibold hover:bg-yellow-700  betelement hvr-bob">Home</Link>
-            <Link style={{ textDecoration: 'none' }} to="code" className="mx-3 bg-yellow-200 rounded-md px-2 py-1 text-black font-semibold hover:bg-yellow-700  betelement hvr-bob">Code</Link>
-            <Link style={{ textDecoration: 'none' }} to="ritual" className="mx-3 bg-yellow-200 rounded-md px-2 py-1 text-black font-semibold hover:bg-yellow-700  betelement hvr-bob">Ritual</Link>
+            <Link style={{ textDecoration: 'none' }} to="home" className="bg-yellow-200 rounded-md px-2 py-1 text-black font-semibold hover:bg-yellow-700  betelement hvr-bob">Home</Link>
+            <Link style={{ textDecoration: 'none' }} to="code" className=" bg-yellow-200 rounded-md px-2 py-1 text-black font-semibold hover:bg-yellow-700  betelement hvr-bob">Code</Link>
+            <Link style={{ textDecoration: 'none' }} to="ritual" className=" bg-yellow-200 rounded-md px-2 py-1 text-black font-semibold hover:bg-yellow-700  betelement hvr-bob">Ritual</Link>
             
             
             </div>
 
-            <div className="left-0">
+            <div className="justify-center items-center">
             <Link style={{ textDecoration: 'none' }} to="join_us" className="mx-3 bg-yellow-200 rounded-md px-2 py-1 text-black font-semibold hover:bg-yellow-700  join betelement hvr-icon-bob hvr-icon">Join<span>&#8594;</span></Link>
             </div>
             
